f8a6418af727aeda8d4b9ea8a77bbeb7323bfc7f
AnnaRabychina <nutochka@tut.by>
2022-06-03 13:27:12 +0300

    feat: implement copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,19 @@
+import  {rename as moveFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import  path, { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const currentFilesPath = path.join(__dirname, 'files');
+const newFilesPath = path.join(__dirname, 'files_copy')
+
 export const copy = async () => {
-    // Write your code here 
+    try {
+        await moveFile(currentFilesPath, newFilesPath);
+        console.log(`Moved files from ${currentFilesPath} to ${newFilesPath}`);
+      } catch (error) {
+        console.error('FS operation failed');
+      }
 };
+
+copy();
