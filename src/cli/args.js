024fa9eca17c73a89242059f1b59e9aa02e6c9e2
AnnaRabychina <nutochka@tut.by>
2022-06-05 10:40:41 +0300

    feat: implement env.js adn args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,10 @@
+// node src\cli\args --propName value --propName2 value2
+
 export const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    for (let i = 0; i< args.length; i=i+2) {
+        console.log(`${args[i]} is ${args[i+1]};`);
+    }
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,12 @@
+// for test bash: RSS_name1=value1 RSS_name2=value2 node src/cli/env
+// for test powershell: $env:RSS_name1="value1"; $env:RSS_name2="value2"; node src/cli/env
+
 export const parseEnv = () => {
-    // Write your code here 
+    const args=process.env;
+    const filteredByKey = Object.fromEntries(Object.entries(args).filter(([key, value]) => key.startsWith('RSS_'))); 
+    for (let [key, value] of Object.entries(filteredByKey)) {
+        console.log(`${key}=${value};`);
+    }
 };
+
+parseEnv();
